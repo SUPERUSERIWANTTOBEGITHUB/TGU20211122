277a4303c130e99415605b28c69043a2990cb635
AS <yvg68@yandex.ru>
2021-11-23 19:41:05 +0300

    tht 3 commit

--- app/src/main/java/com/measurer/tgu_crc1/MainActivity.kt
+++ app/src/main/java/com/measurer/tgu_crc1/MainActivity.kt
@@ -2,10 +2,13 @@ package com.measurer.tgu_crc1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// my first comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+// my second comment
+//my thirth comment
